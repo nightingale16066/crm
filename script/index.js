3679f7033d63ef285df10067a0054d58d3e28d68
nightingale16066 <nightingale16066@gmail.com>
2022-08-27 23:25:00 +0300

    get all required elements

---
+++ script/index.js
@@ -0,0 +1,5 @@
+const modalTitle = document.querySelector('.modal__title');
+const modalForm = document.querySelector('.modal__form');
+const modalCheckbox = document.querySelector('.modal__checkbox');
+const modalInputDiscount = document.querySelector('.modal__input_discount');
+
